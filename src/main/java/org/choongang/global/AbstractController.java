a295b4c655d2cdd7b3027116e91d4797e137d8d8
heyejiyang <xoxo99312@naver.com>
2024-05-27 16:59:06 +0900

    회원가입기능 구현중...

--- src/main/java/org/choongang/global/AbstractController.java
+++ src/main/java/org/choongang/global/AbstractController.java
@@ -5,6 +5,7 @@ import org.choongang.main.MainRouter;
 import org.choongang.template.Templates;
 
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public abstract class AbstractController implements Controller {
     //템플릿 메서드 패턴 구현
@@ -33,7 +34,7 @@ public abstract class AbstractController implements Controller {
     * - 숫자: 메뉴 항목
     * */
     public void prompt(){
-        System.out.println(Templates.getInstance().dpubleLine());
+        //System.out.println(Templates.getInstance().dpubleLine());
         System.out.print("메뉴 선택: ");
         String menu = sc.nextLine();
         if(menu.equals("q")|| menu.equals("quit")||menu.equals("exit")){
@@ -64,5 +65,21 @@ public abstract class AbstractController implements Controller {
         }
         //메뉴 컨트롤러 변경 처리 -> Router 구현체에 넣어줌
         MainRouter.getInstance().change(menu);
+    }
+
+    /*
+    입력과 검증을 함께 진행
+    * @param message: 항목 메시지
+    * @param predicate: 판별식
+    * @return
+    * */
+    protected String propmtWithValidation(String message, Predicate<String> predicate){
+        String str = null;
+        do{
+            System.out.print(message);
+            str = sc.nextLine();
+        }while (!predicate.test(str)); //판별식이 실패했을 경우 계속 반복
+
+        return str;
     }
 }
